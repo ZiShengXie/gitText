fff039df1b66bbe085c159797f520d3b416f8556
ZiSheng Xie <313823018@qq.com>
2017-10-15 17:27:28 +0800

    新分支上传文件

---
+++ gitText/NewBranchFile.h
@@ -0,0 +1,13 @@
+//
+//  NewBranchFile.h
+//  gitText
+//
+//  Created by zisheng xie on 2017/10/15.
+//  Copyright © 2017年 zisheng xie. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NewBranchFile : NSObject
+
+@end
---
+++ gitText/NewBranchFile.m
@@ -0,0 +1,13 @@
+//
+//  NewBranchFile.m
+//  gitText
+//
+//  Created by zisheng xie on 2017/10/15.
+//  Copyright © 2017年 zisheng xie. All rights reserved.
+//
+
+#import "NewBranchFile.h"
+
+@implementation NewBranchFile
+
+@end
